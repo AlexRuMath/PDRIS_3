db054dfed58ca1e5ded73e6bd1fd357ac480d98b
AlexRuMath <bioshock20129@gmail.com>
2022-11-29 13:45:24 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
 docker stop app_container
-git clone https://github.com/AlexRuMath/PDRIS_3 -b main
-cd ~/PDRIS_3
-ls .
 docker build -t alexrumath/app .
 docker run -d -p 3000:3000 alexrumath/app --name app_container
