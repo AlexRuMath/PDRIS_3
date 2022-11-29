3e07385c4bd86e54755b36e7ae27719808425de5
AlexRuMath <bioshock20129@gmail.com>
2022-11-29 13:38:44 +0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-docker stop happy_germain
+docker stop app_container
 git clone https://github.com/AlexRuMath/PDRIS_3 -b main
 cd ~/PDRIS_3
 docker build -t alexrumath/app .
-docker run -p 3000:3000 alexrumath/app
+docker run -d -p 3000:3000 alexrumath/app --name app_container
