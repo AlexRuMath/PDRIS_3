66fdc77ab62c30b410a74f51633aa9283d421df7
AlexRuMath <bioshock20129@gmail.com>
2022-11-29 13:58:15 +0300

    afsf

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker stop app_container
 docker build -t alexrumath/app .
-docker run -d -p 3000:3000 alexrumath/app --name app_container
+docker run -p 3000:3000 alexrumath/app --name app_container
