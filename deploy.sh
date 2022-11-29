dbe77383a9ef0a7348ef9595451f1ec41279ad43
AlexRuMath <bioshock20129@gmail.com>
2022-11-29 13:59:35 +0300

    123

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker stop app_container
 docker build -t alexrumath/app .
-docker run -p 3000:3000 alexrumath/app --name app_container
+docker run --name app_container -p 3000:3000 alexrumath/app 
